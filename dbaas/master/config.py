a2d4631325d4e5d0152d106c7e44865b1bde6c7d
G0uth4m <goutham67r@gmail.com>
2020-03-26 16:59:59 +0530

    rpc implemented

--- dbaas/master/config.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://mongomaster:27017/")
-db = client["rideshare"]
-
-rabbitmq_hostname = "rabbitmq"
--- dbaas/slave/config.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://mongoslave:27017/")
-db = client["rideshare"]
-
-rabbitmq_hostname = "rabbitmq"
