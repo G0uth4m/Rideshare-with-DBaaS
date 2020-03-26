8e2690d7c89f66e514879733aebb786412312fd7
G0uth4m <goutham67r@gmail.com>
2020-03-26 17:00:15 +0530

    rpc implemented

---
+++ dbaas/worker/config.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+import os
+
+db_hostname = os.environ['DB_HOSTNAME']
+client = MongoClient("mongodb://" + db_hostname + ":27017/")
+db = client["rideshare"]
+
+rabbitmq_hostname = "rabbitmq"
